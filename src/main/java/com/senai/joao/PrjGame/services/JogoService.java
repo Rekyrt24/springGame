96bbfe25733aaee8e2e996f23cc812d92c2109e5
João Victor <133990552+Rekyrt24@users.noreply.github.com>
2023-10-19 07:39:54 -0300

    Add files via upload

---
+++ src/main/java/com/senai/joao/PrjGame/services/JogoService.java
@@ -0,0 +1,35 @@
+package com.senai.joao.PrjGame.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.senai.joao.PrjGame.entities.Jogo;
+import com.senai.joao.PrjGame.repositories.JogoRepository;
+
+@Service
+public class JogoService {
+	private final JogoRepository jogoRepository;
+
+	@Autowired
+	public JogoService(JogoRepository jogoRepository) {
+		this.jogoRepository = jogoRepository;
+	}
+
+	public Jogo saveJogo(Jogo jogo) {
+		return jogoRepository.save(jogo);
+	}
+
+	public Jogo getJogoById(Long id) {
+		return jogoRepository.findById(id).orElse(null);
+	}
+
+	public List<Jogo> getAllJogos() {
+		return jogoRepository.findAll();
+	}
+
+	public void deleteJogo(Long id) {
+		jogoRepository.deleteById(id);
+	}
+}
